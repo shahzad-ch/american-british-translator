d0d60f70cc90dcd893144402de103808b222eb46
shahzad-ahmed <shahzadwarraich.sc@gmail.com>
2024-06-14 02:19:23 +0500

    fix: bugs

--- components/translator.js
+++ components/translator.js
@@ -4,13 +4,7 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
-    britishToAmerican(text) {
-        const britishToAmericanSpelling = Object.fromEntries(Object.entries(americanToBritishSpelling).map(([key, val]) => [val, key]))
-
-        const dict = { ...britishToAmericanSpelling, ...britishOnly }
-        let arr = text.split(' ');
-
-
+    translateSpelling(arr, dict) {
         arr.map((word, index) => {
 
             const str = word.match(/[a-zA-Z:0-9']+/)
@@ -26,6 +20,17 @@ class Translator {
 
             arr[index] = front + newWord + back
         })
+
+        return arr;
+    }
+    britishToAmerican(text) {
+        const britishToAmericanSpelling = Object.fromEntries(Object.entries(americanToBritishSpelling).map(([key, val]) => [val, key]))
+
+        const dict = { ...britishToAmericanSpelling, ...britishOnly }
+        let arr = text.split(' ');
+
+        arr = this.translateSpelling(arr, dict)
+        
 
         arr.map((word, index) => {
             for (let x in americanToBritishTitles) {
@@ -66,22 +71,7 @@ class Translator {
         let arr = text.split(' ');
 
         // console.log(arr)
-        arr.map((word, index) => {
-
-            let front; let back;
-            const str = word.match(/[a-zA-Z:0-9']+/)
-            front = word.slice(0, str.index)
-            back = word.slice(str[0].length + front.length)
-            let newWord = str[0];
-
-            for (let x in dict) {
-                if (str[0].toLowerCase() == x) {
-                    newWord = `<span class="highlight">${dict[x]}</span>`
-                }
-            }
-
-            arr[index] = front + newWord + back
-        })
+        arr = this.translateSpelling(arr, dict)
 
         arr.map((word, index) => {
             for (let x in americanToBritishTitles) {
@@ -106,11 +96,9 @@ class Translator {
             }
         }
 
-        new_str = new_str.replace(/(\d+):(\d+)/g, '<span class="highlight">$1:$2</span>')
+        new_str = new_str.replace(/(\d+):(\d+)/g, '<span class="highlight">$1.$2</span>')
         // str = new_arr.join(' ')
         console.log(new_str)
-        // return (new_arr == [] ? arr.join('') : new_arr.join('') )
-        // console.log(arr.join(''))
         return new_str
 
     }
